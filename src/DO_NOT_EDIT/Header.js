3d5e6a212c0c20775ca54be5b1bbc95bce63385e
Emmanuel Olivo <emmanuel@wealthcharts.com>
2022-02-02 15:55:58 -0600

    intial markup with example json

---
+++ src/DO_NOT_EDIT/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mb-8">
+      <h1 className="text-center text-4xl md:text-6xl lg:text-6xl text-yellow-300 font-bold uppercase">
+        Pokémon Search
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
