3cd20dc6cbbf64ca00571261e8ba96fc7b3e402f
Emmanuel Olivo <emmanuel@wealthcharts.com>
2022-02-07 10:07:32 -0600

    Finish example response

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //! Imports - DO NOT EDIT THESE FILES
+import { useState } from "react";
 import Header from "./DO_NOT_EDIT/Header";
 import PokemonDataPanel from "./DO_NOT_EDIT/PokemonDataPanel";
 import PokemonNotFound from "./DO_NOT_EDIT/PokemonNotFound";
 import PokemonStatProgressBar from "./DO_NOT_EDIT/PokemonStatProgressBar";
 import PokemonTypeBadge from "./DO_NOT_EDIT/PokemonTypeBadge";
-import pokemonData from "./example.json";
+import useGetPokemonData from "./useGetPokemonData";
 //! Imports - DO NOT EDIT THESE FILES
 
 /* 
@@ -31,6 +32,20 @@ import pokemonData from "./example.json";
 // import useGetPokemonData from "./useGetPokemonData";
 
 function App() {
+  const [pokemonValue, setPokemonValue] = useState("");
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+
+  const { pokemonData, error } = useGetPokemonData(selectedPokemon);
+
+  const onSearch = () => {
+    setSelectedPokemon(pokemonValue);
+  };
+  const onRandom = () => {
+    // Get a random number between 1 and 898
+    const randomNumber = Math.floor(Math.random() * 898) + 1;
+    setSelectedPokemon(randomNumber);
+  };
+
   return (
     <main>
       <div className="bg-blue-900 pb-8 pt-16">
@@ -40,75 +55,65 @@ function App() {
           {/* Search Field and Button */}
           <div className="search-input">
             {/* THE VALUE OF THIS INPUT WILL BE USED AS THE SEARCH PARAMETER */}
-            <input type="text" placeholder="Type name or number (eg. Bulbasaur, 35)" />
+            <input
+              type="text"
+              placeholder="Type name or number (eg. Bulbasaur, 35)"
+              value={pokemonValue}
+              onChange={e => setPokemonValue(e.target.value)}
+              onKeyDown={e => {
+                if (e.key === "Enter") {
+                  onSearch();
+                }
+              }}
+            />
 
             {/* CLICKING THIS BUTTON SHOULD FIRE THE API REQUEST WITH THE QUERY IN THE INPUT */}
-            <button>Search</button>
+            <button onClick={() => onSearch()}>Search</button>
           </div>
 
           <p className="text-white"> - OR - </p>
 
           {/* CLICKING THIS BUTTON SHOULD FIRE THE API REQUEST WITH A RANDOM POKEMON */}
-          <button className="random-button">Get Random</button>
+          <button className="random-button" onClick={() => onRandom()}>
+            Get Random
+          </button>
         </div>
       </div>
 
       <div className="data-container">
-        {/* 
-        
-        THIS IS WHERE YOU NEED TO DISPLAY THE DATA OF THE POKEMON IN THE DIFFERENT PANELS 
-        POSITIONED IN A GRID LIKE THE EXAMPLE IMAGE:
+        {pokemonData !== null && !error ? (
+          <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-6 gap-2">
+            <PokemonDataPanel className="row-span-1 row-start-1 col-span-1  rounded-lg flex items-center">
+              <h3 className="text-xl uppercase font-bold">{pokemonData.name}</h3>
+            </PokemonDataPanel>
+            <PokemonDataPanel className="row-span-1 row-start-1 col-span-1  rounded-lg flex items-center">
+              <h3 className="text-lg font-bold">#{pokemonData.id}</h3>
+            </PokemonDataPanel>
 
-        https://imgur.com/a/X47AYqn
+            <PokemonDataPanel className="row-start-2 row-span-4  col-span-2  rounded-lg flex justify-center">
+              <img
+                src={pokemonData.sprites.front_default}
+                alt={pokemonData.name}
+                className="object-cover w-40 md:h-full md:w-auto"
+              />
+            </PokemonDataPanel>
 
-        To display the type badges, use the PokemonTypeBadge component, which is already imported.
-        This component takes a typeName as a prop, which is a string like "fire", "water", "electric", "bug", etc.
-        REMEMBER THAT POKEMON CAN HAVE ONE OR TWO TYPES.
+            <PokemonDataPanel className="row-start-6 row-span-1 col-start-1 col-span-2   py-2 px-4 rounded-lg flex items-center justify-center gap-2">
+              {pokemonData.types.map(({ type }) => (
+                <PokemonTypeBadge key={type.name} typeName={type.name} />
+              ))}
+            </PokemonDataPanel>
 
-        To display the stat progress bars, use the PokemonStatProgressBar component, which is also imported.
-        This component takes a statName and baseStat as props:
-        statName which is a string like "hp", "attack", "defense", etc.
-        baseStat which is an integer like 50, 100, 200, etc.
-
-         */}
-
-        {/* Pokemon Data */}
-        <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-6 gap-2">
-          <PokemonDataPanel className="row-span-1 row-start-1 col-span-1  rounded-lg flex items-center">
-            <h3 className="text-xl uppercase font-bold">{pokemonData.name}</h3>
-          </PokemonDataPanel>
-          <PokemonDataPanel className="row-span-1 row-start-1 col-span-1  rounded-lg flex items-center">
-            <h3 className="text-lg font-bold">#{pokemonData.id}</h3>
-          </PokemonDataPanel>
-
-          <PokemonDataPanel className="row-start-2 row-span-4  col-span-2  rounded-lg flex justify-center">
-            <img
-              src={pokemonData.sprites.front_default}
-              alt={pokemonData.name}
-              className="object-cover w-40 md:h-full md:w-auto"
-            />
-          </PokemonDataPanel>
-
-          <PokemonDataPanel className="row-start-6 row-span-1 col-start-1 col-span-2   py-2 px-4 rounded-lg flex items-center justify-center gap-2">
-            {pokemonData.types.map(({ type }) => (
-              <PokemonTypeBadge typeName={type.name} />
-            ))}
-          </PokemonDataPanel>
-
-          <PokemonDataPanel className="col-span-2 md:row-start-2 md:row-span-full   rounded-lg ">
-            <h4 className="text-lg font-bold mb-4">Stats:</h4>
-            {pokemonData.stats.map(({ base_stat, stat }) => (
-              <PokemonStatProgressBar statName={stat.name} baseStat={base_stat} />
-            ))}
-          </PokemonDataPanel>
-        </div>
-
-        {/* 
-
-          THIS COMPONENT SHOULD BE DISPLAYED WHERE NO POKEMON DATA WAS RETURNED FROM THE QUERY
-
-        */}
-        {/* <PokemonNotFound /> */}
+            <PokemonDataPanel className="col-span-2 md:row-start-2 md:row-span-full   rounded-lg ">
+              <h4 className="text-lg font-bold mb-4">Stats:</h4>
+              {pokemonData.stats.map(({ base_stat, stat }) => (
+                <PokemonStatProgressBar key={stat.name} statName={stat.name} baseStat={base_stat} />
+              ))}
+            </PokemonDataPanel>
+          </div>
+        ) : (
+          <PokemonNotFound />
+        )}
       </div>
     </main>
   );
--- src/useGetPokemonData.js
+++ src/useGetPokemonData.js
@@ -10,3 +10,29 @@ GET https://pokeapi.co/api/v2/pokemon/{string | number}
 Documentation available here https://pokeapi.co/
 
 */
+
+import { useEffect, useState } from "react";
+
+const useGetPokemonData = query => {
+  const [pokemonData, setPokemonData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+        const json = await response.json();
+        setPokemonData(json);
+        setError(null);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    if (query) fetchData();
+  }, [query]);
+
+  return { pokemonData, error };
+};
+
+export default useGetPokemonData;
